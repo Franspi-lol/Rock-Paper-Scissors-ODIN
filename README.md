2577f5e6b1b7172a8b221a844f9de0fb3b928785
Franspi-lol <francobernar@gmail.com>
2024-04-21 20:07:26 -0300

    Ui added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Rock-Paper-Scissors-ODIN
 This is a working javascript game of Rock Paper Scissors
+Now with UI!
 I did it as part of the ODIN Project
--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-const rock = 0;
+/* const rock = 0;
 const paper = 1;
-const scissor = 2;
+const scissor = 2; */
 const MAX = 3;
 
 function getComputerChoice() {
@@ -28,28 +28,25 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-
 /* playerSelection = playerSelection.toLowerCase(); */
 
 function getPLayerSelection() {
-  
-  
-  var check = false
-  while (check== false)
-  {
+  var check = false;
+  while (check == false) {
     var playerSelection = prompt("Rock Paper or Scissor");
     playerSelection = playerSelection.toLowerCase();
     if (
-        playerSelection === "rock" ||
-        playerSelection === "scissor" ||
-        playerSelection === "paper"
-      ) {
-        check = true
-      }
+      playerSelection === "rock" ||
+      playerSelection === "scissor" ||
+      playerSelection === "paper"
+    ) {
+      check = true;
+    }
   }
   return playerSelection;
-  
 }
+
+var resultadoInd = document.getElementById("resultado");
 
 function playRound(playerSelection, computerChoice) {
   if (typeof playerSelection === "string") {
@@ -61,34 +58,35 @@ function playRound(playerSelection, computerChoice) {
   var draw = "It's a Tie";
   var areEqual = playerSelection === computerChoice;
   if (areEqual) {
-    console.log(draw);
+    resultadoInd.textContent = draw;
+
     return 1;
   }
   switch (playerSelection) {
     case "rock":
       if (computerChoice === "scissor") {
-        console.log(win.concat(" Rock beats Scissor!"));
+        resultadoInd.textContent = win.concat(" Rock beats Scissor!");
         return 0;
       } else {
-        console.log(lose.concat(" Paper beats Rock!"));
+        resultadoInd.textContent = lose.concat(" Paper beats Rock!");
         return 2;
       }
       break;
     case "paper":
       if (computerChoice === "rock") {
-        console.log(win.concat(" Paper beats Rock!"));
+        resultadoInd.textContent = lose.concat(" Paper beats Rock!");
         return 0;
       } else {
-        console.log(lose.concat(" Scissor beats Paper!"));
-        return 2 ;
+        resultadoInd.textContent =lose.concat(" Scissor beats Paper!");
+        return 2;
       }
       break;
     case "scissor":
       if (computerChoice === "paper") {
-        console.log(win.concat(" Scissor beats Paper!"));
+        resultadoInd.textContent =lose.concat(" Scissor beats Paper!");
         return 0;
       } else {
-        console.log(lose.concat(" Rock beats Scissor!"));
+        resultadoInd.textContent = win.concat(" Rock beats Scissor!");
         return 2;
       }
       break;
@@ -96,36 +94,80 @@ function playRound(playerSelection, computerChoice) {
       break;
   }
 }
-/* console.log(playRound(getPLayerSelection(), getComputerChoice())); */
-/* playRound(getPLayerSelection(), getComputerChoice()) */
-console.log(playGame());
-function playGame()
-{
-    var playerCount = 0
-    var comCount = 0
-    for (let index = 0; index < 5; index++) {
-        var result = playRound(getPLayerSelection(), getComputerChoice())
-        if (result==0) {
-            playerCount++
-        }else
-        {
-            if (result==2) {
-                comCount++
-            }
-        }
+
+function playGame() {
+  var playerCount = 0;
+  var comCount = 0;
+  for (let index = 0; index < 5; index++) {
+    var result = playRound(getPLayerSelection(), getComputerChoice());
+    if (result == 0) {
+      playerCount++;
+    } else {
+      if (result == 2) {
+        comCount++;
+      }
     }
-    if (playerCount>comCount)
+  }
+  if (playerCount > comCount) {
+    return "Player Wins!";
+  } else {
+    if (playerCount < comCount) {
+      return "Computer Wins!";
+    } else {
+      return "It's a Tie! Refresh and try to Win!";
+    }
+  }
+}
+
+function playAndShow(playerChoice)
+{
+  var resultado = playRound(playerChoice, getComputerChoice());
+  if(resultado == 2){
+    resultadoPlayer++;
+  }else
+  {
+    if(resultado == 0)
     {
-        return "Player Wins!"
+      resultadoPC++
+    }
+  }
+  global.innerText = resultadoPlayer;
+  globalPC.innerText = resultadoPC;
+  if(resultadoPlayer==5||resultadoPC==5)
+  {
+    if(resultadoPlayer==5){
+      alert('You Win!');
     }else
     {
-        if(playerCount<comCount)
-        {
-            return "Computer Wins!"
-        }
-        else
-        {
-            return "It's a Tie! Refresh and try to Win!" 
-        }
+      alert('You Lose!')
     }
+    resultadoPC=0;
+    resultadoPlayer=0;
+    global.innerText = resultadoPlayer;
+  globalPC.innerText = resultadoPC;
+  }
 }
+
+var rock = document.getElementById("rock");
+var paper = document.getElementById("paper");
+var scissor = document.getElementById("scissor");
+var global = document.getElementById('global');
+var globalPC = document.getElementById('global2');
+
+
+
+var resultadoPC = 0;
+var resultadoPlayer = 0;
+
+rock.addEventListener("click", () => {
+  playAndShow(rock.id)
+});
+
+
+
+paper.addEventListener("click", () => {
+  playAndShow(paper.id)
+});
+scissor.addEventListener("click", () => {
+  playAndShow(scissor.id)
+});
